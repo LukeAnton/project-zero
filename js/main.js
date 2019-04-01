771651a9a6e2b6053ff52bb3030dfa426fb6aa60
Luke Firth <lukeantonfirth@gmail.com>
2019-04-01 15:28:53 +1100

    added press logic and game board

---
+++ js/main.js
@@ -0,0 +1,42 @@
+console.log('running');
+
+
+$(function(){
+
+$('.a').click(function(){
+    $('.a').html(`<img src="svg/o.svg" alt="">`)
+});
+
+$('.b').click(function(){
+    $('.b').html(`<img src="svg/x.svg" alt="">`)
+});
+
+$('.c').click(function(){
+    $('.c').html(`<img src="svg/o.svg" alt="">`)
+});
+
+$('.d').click(function(){
+    $('.d').html(`<img src="svg/x.svg" alt="">`)
+});
+
+$('.e').click(function(){
+    $('.e').html(`<img src="svg/o.svg" alt="">`)
+  });
+
+$('.f').click(function(){
+    $('.f').html(`<img src="svg/x.svg" alt="">`)
+});
+
+$('.g').click(function(){
+    $('.g').html(`<img src="svg/o.svg" alt="">`)
+});
+
+$('.h').click(function(){
+    $('.h').html(`<img src="svg/x.svg" alt="">`)
+});
+
+$('.i').click(function(){
+    $('.i').html(`<img src="svg/o.svg" alt="">`)
+});
+
+});
